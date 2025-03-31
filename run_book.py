58a1959e4ae06b929fdeb3cc9525502491e2160d
Jens Köster <jens.koster@issuu.com>
2025-03-31 19:36:00 +0200

    minor fixes

--- run_book.py
+++ run_book.py
@@ -5,13 +5,11 @@ import os
 
 import papermill as pm
 
-# Define the parameters
-parameters = {"execution_hour_str": "2025-03-12 10:00:00", "output_root": "./output", "overlap_hours": 3}
-
-notebook = "extract_wikipedia_pageviews"
+notebook = "helloworld"
 # Path to the input notebook
 input_notebook = f"notebooks/{notebook}.ipynb"
-
+parameters = {"p1": "p2", "p2": "p1"}
+# parameters = {"execution_hour_str": "2025-03-12 10:00:00", "output_root": "./output", "overlap_hours": 3}
 
 # Create the output directory if it doesn't exist
 output_dir = f"./output/{notebook}"
@@ -19,6 +17,7 @@ os.makedirs(output_dir, exist_ok=True)
 output_notebook = f"{output_dir}/{notebook}{dt.datetime.now()}.ipynb"
 
 # Run the notebook with the specified parameters
+
 pm.execute_notebook(input_path=input_notebook, output_path=output_notebook, parameters=parameters)
 
 print(f"Notebook executed and saved to {output_notebook}")
